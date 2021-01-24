dd316dd9c3fd7e59b06ffd3213aaa2bd607026a1
nokada11 <naoki.okada@tufts.edu>
2021-01-24 04:05:52 -0500

    Dashboard and learning almost done

--- components/MeetingWidget/index.tsx
+++ components/MeetingWidget/index.tsx
@@ -1,9 +1,28 @@
 import styles from './MeetingWidget.module.scss';
+import Checkin from '../Checkin';
 
 const DescriptionWidget: React.FC = () => {
   return(
     <div className={`${styles.widget}`}>
-      <p>Meetings</p>
+      
+      <div className={styles.columns}>
+        <div className={styles.title}>
+          <p><b>Meetings</b></p>
+        </div>
+        <div>
+          <p>Sort by mango ∨</p>
+        </div>
+        <div>
+        <a className={`button is-link ${styles.button}`} href="/dashboard">+Add</a>
+        </div>
+      </div>
+      <div className={styles.meetingEntry}>
+        <Checkin  date={new Date()} user={{icon: '', name: 'Jason McConnell'}} />
+      </div>
+
+      <div className={styles.meetingEntry}>
+        <Checkin  date={new Date()} user={{icon: '', name: 'Mitch Goldmann'}} />
+      </div>
     </div>
   )
 }
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -4,6 +4,8 @@ import SideBar from '../components/DashboardSideBar/index';
 import WelcomeWidget from '../components/WelcomeWidget/index';
 import LearningWidget from '../components/LearningWidget/index';
 import DescriptionWidget from '../components/DescriptionWidget/index';
+import ActivityWidget from '../components/ActivityWidget/index';
+import UpcomingWidget from '../components/UpcomingWidget/index';
 import MeetingWidget from '../components/MeetingWidget/index';
 import styles from '../styles/dashboard.module.scss';
 
@@ -14,21 +16,18 @@ const Dashboard: React.FC = () => {
         <SideBar/>
         <div className='column'>
           <TopBar/>
-
           <div className={styles.widgets}>
             <div className='columns'>
               <div className='column'>
                 <WelcomeWidget/>
                 <p className={`${styles.midSpace}`}><b>Progress</b></p>
-                <div className={`columns ${styles.smallSpace} ${styles.alignLeft}`}>
-                  <LearningWidget/>
-                  <div className='column'>
-                    <div className={`${styles.widget} ${styles.whiteBackground}`}>
-                    
-                    </div>
-                    <div className={`${styles.widget} ${styles.whiteBackground}`}>
-                    
-                    </div>
+                <div className={`${styles.columns} ${styles.smallSpace}`}>
+                  <div className={styles.column}>
+                    <LearningWidget/>
+                  </div>
+                  <div>
+                    <ActivityWidget/>
+                    <UpcomingWidget/>
                   </div>
                 </div>
                 
