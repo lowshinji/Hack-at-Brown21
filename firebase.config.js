f950e2e75581adc70797c451da88a7e3457f2ef7
lowshinji <lowshinji@gmail.com>
2021-01-24 18:04:40 +0000

    Create firebase.config.js

---
+++ firebase.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyDkzRXK2otEjH265Uhqldy9h0GGt1RQ3hY",
+  authDomain: "digieduhack-cbeb7.firebaseapp.com",
+  databaseURL: "https://digieduhack-cbeb7.firebaseio.com",
+  projectId: "digieduhack-cbeb7",
+  storageBucket: "digieduhack-cbeb7.appspot.com",
+  messagingSenderId: "521447385123",
+  appId: "1:521447385123:web:9577cf2bfe83a6f0ece996",
+  measurementId: "G-7W5SF0MZ35",
+};
