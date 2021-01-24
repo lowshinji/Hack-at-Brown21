45d84b1d041a4b108697cfc5fd32898191493c10
Shin Ji Low <lowshinji@gmail.com>
2021-01-24 00:17:33 +0000

    Added design page

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -13,6 +13,7 @@ const Layout: React.FC<Props> = ({ children, title = 'This is the default title'
       <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+			<link rel="stylesheet" href="https://use.typekit.net/htq3msi.css" />
     </Head>
     <header>
       <nav>
