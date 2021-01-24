11cbb0a21fc295cdf2811a5c2130c9c446ff5634
dylanOshima <dylanoshima@gmail.com>
2021-01-24 22:42:54 +0800

    Updates typography task

--- components/LearningTryYourself/Lesson3.tsx
+++ components/LearningTryYourself/Lesson3.tsx
@@ -11,12 +11,12 @@ type Lesson3Type = {
  * @param props 
  */
 const Lesson3: React.FC<Lesson3Type> = () => {
-  const [font, setFont] = useState('Arial');
+  const [font, setFont] = useState('Comic Sans MS');
   const [fontSize, setFontSize] = useState(50);
-  const [tracking, setTracking] = useState(50);
+  const [tracking, setTracking] = useState(-10);
 
   const success_condition = (
-    fontSize === 32 && font === 'Helvetica' && tracking === 50
+    fontSize === 32 && font === 'Helvetica' && tracking === 4
   )
 
   return (
@@ -29,6 +29,7 @@ const Lesson3: React.FC<Lesson3Type> = () => {
               background: '#404380',
               border: "none",
               borderRadius: '10px',
+              letterSpacing: tracking,
               font: `${fontSize}px "${font}"`,
               fontWeight: 500
             }}>
@@ -44,16 +45,17 @@ const Lesson3: React.FC<Lesson3Type> = () => {
           <label>Font Family: </label>
           <select className={style.selector} onChange={(e) => setFont(e.target.value)} defaultValue={font}>
             <option value="Arial">Arial</option>
-            <option value="Times New Roman">Times New Roman</option>
+            <option value="Comic Sans MS">Comic Sans MS</option>
             <option value="Bauhaus">Bauhaus</option>
-            <option value="Helvetica">Helvetica</option>
+            <option value="Impact">Impact</option>
+            <option value="Roboto">Roboto</option>
           </select>
         </div>
         <div className={style.controller}>
           <label>Tracking: </label>
           <div>
-            0 <input type="range" min="1" max="100" value={tracking} className={style.slider} onChange={(e) => setTracking(parseInt(e.target.value,10))}></input> 100
-            <input type="number" min="1" max="100" value={tracking} className={style.slider_input} onChange={(e) => setTracking(parseInt(e.target.value,10))}></input> 
+            -50 <input type="range" min="-50" max="50" value={tracking} className={style.slider} onChange={(e) => setTracking(parseInt(e.target.value,10))}></input> 50
+            <input type="number" min="-50" max="50" value={tracking} className={style.slider_input} onChange={(e) => setTracking(parseInt(e.target.value,10))}></input> 
           </div>
         </div>
         <div className={style.controller}>
